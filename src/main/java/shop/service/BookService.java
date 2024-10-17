b9a8ac8619b1c9840549fe0166d3ae88cad49f9f
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2024-10-17 14:06:00 +0200

    Pagination, Sorting, and Swagger 1.0 added pagination, sorting, swagger to the controller

--- src/main/java/shop/service/BookService.java
+++ src/main/java/shop/service/BookService.java
@@ -1,6 +1,7 @@
 package shop.service;
 
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import shop.dto.request.BookSearchParamsDto;
 import shop.dto.request.CreateBookRequestDto;
 import shop.dto.request.UpdateBookRequestDto;
@@ -11,11 +12,11 @@ public interface BookService {
 
     BookDto getBookById(Long id);
 
-    List<BookDto> getAllBooks();
+    List<BookDto> getAllBooks(Pageable pageable);
 
     BookDto updateBookById(Long id, UpdateBookRequestDto updateBookDto);
 
     void deleteBookById(Long id);
 
-    List<BookDto> search(BookSearchParamsDto searchParamsDto);
+    List<BookDto> search(BookSearchParamsDto searchParamsDto, Pageable pageable);
 }
